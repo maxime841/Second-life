400bf7e8721656d241046c3e6709ead68f285738
maxime841 <maxime.poirot@gmail.com>
2022-06-21 15:17:25 +0200

    page home sans carte avec footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,50 @@
-// == Import
 import React from 'react';
-import { Footer } from 'react-bootstrap';
+import {
+  CDBBox, CDBBtn,
+} from 'cdbreact';
+import facebook from '../../../public/image/logo_facebook.png';
+import twitter from '../../../public/image/logo_twitter.png';
+import instagram from '../../../public/image/logo_instagram.jpeg';
+import logoSecondLife from '../../../public/image/logosecondlife.jpg';
 
-// == Composant
-const Myfooter = () => (
-  <Footer bg="dark" variant="dark">
-    <p>dsfdsf</p>
-  </Footer>
+//= => import style
+import './footer.scss';
+
+const Footer = () => (
+  <div className="footer">
+    <CDBBox
+      display="flex"
+      justifyContent="between"
+      alignItems="center"
+      className="mx-auto py-4 flex-wrap"
+      style={{ width: '80%' }}
+    >
+      <CDBBox display="flex" alignItems="center">
+        <a href="/" className="d-flex align-items-center p-0 text-dark">
+          <img
+            alt="logo second life"
+            src={logoSecondLife}
+            width="30px"
+          />
+          <span className="ml-4 h5 mb-0 font-weight-bold">Second life</span>
+        </a>
+      </CDBBox>
+      <CDBBox>
+        <small className="ml-2">&copy; second life, 2022. All rights reserved.</small>
+      </CDBBox>
+      <CDBBox display="flex">
+        <CDBBtn flat color="dark" className="p-2 button">
+          <img className="footer-logo" alt="Facebook icon" srcSet="https://img.icons8.com/fluency/2x/facebook-new.png 2x" />
+        </CDBBtn>
+        <CDBBtn flat color="dark" className="mx-3 p-2 button">
+          <img className="footer-logo" alt="Twitter icon" srcSet="https://img.icons8.com/color/2x/twitter.png 2x" />
+        </CDBBtn>
+        <CDBBtn flat color="dark" className="p-2 button">
+          <img className="footer-logo" srcSet="https://img.icons8.com/color/2x/instagram-new.png 2x" alt="Instagram icon" loading="lazy" />
+        </CDBBtn>
+      </CDBBox>
+    </CDBBox>
+  </div>
 );
 
-// == Export
-export default Myfooter;
+export default Footer;
--- src/components/Secondlife/index.js
+++ src/components/Secondlife/index.js
@@ -8,6 +8,7 @@ import Header from 'src/components/Header';
 import Club from 'src/components/Club';
 import Location from 'src/components/Location';
 import Home from 'src/components/Home';
+import Footer from 'src/components/Footer';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -21,6 +22,7 @@ const Secondelife = () => (
         <Route path="/location" element={<Location />} />
       </Routes>
     </div>
+    <Footer />
   </Router>
 );
 
